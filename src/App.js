63ef7923304b5411219c17702342c567b945fe8b
sanjar <san4o.myrzakmatov@gmail.com>
2023-06-06 17:20:24 -0800

    paidAd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.scss';
 import Header from "./companents/Header";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import Footer from "./companents/Footer";
 import Home from "./companents/Home";
 import Login from "./companents/Login/login";
 import Agents from "./companents/Agents/agents";
+import {useEffect} from "react";
+import FavoriteSetings from "./companents/FavoritesSetings";
+import PaidAd from "./companents/PaidAd";
+import Favorite from "./companents/Favorite";
+
 
 function App() {
+    const {pathname} = useLocation()
+    useEffect(()=>{
+        window.scroll(0,0)
+    },[pathname])
   return (
     <div className="App">
       <Header/>
+        <Favorite/>
       <Routes>
         <Route path={'/'} element={<Home/>}/>
         <Route path={'/login'} element={<Login/>}/>
         <Route path={'/agents'} element={<Agents/>}/>
+        <Route path={'/favoriteSetings'} element={<FavoriteSetings/>}/>
+        <Route path={'/addProperty'} element={<PaidAd/>}/>
       </Routes>
       <Footer/>
     </div>
