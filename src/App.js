9a39403f89d414603d34b7545b56b34a58dc8720
sauda00 <saudakalybekova@gmil.come>
2023-06-08 23:49:43 +0600

    pages

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Footer from "./companents/Footer";
 import Home from "./companents/Home";
 import Login from "./companents/Login/login";
 import Agents from "./companents/Agents/agents";
+import Buy from "./companents/Buy";
+import Dream from "./companents/Buy/detailebuy";
+import Wifi from "./companents/Buy/detailebuy/tv";
+import Rent from "./companents/Buy/Rent";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        {/*<Dream/>*/}
+        {/*<Wifi/>*/}
       <Routes>
         <Route path={'/'} element={<Home/>}/>
         <Route path={'/login'} element={<Login/>}/>
         <Route path={'/agents'} element={<Agents/>}/>
+          <Route path={'/buy'} element={<Buy/>}/>
+          <Route path={'/rent'} element={<Rent/>}/>
+
+
       </Routes>
       <Footer/>
     </div>
