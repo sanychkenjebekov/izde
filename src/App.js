d6f65ffcbe42fb670757993777766292bfdffe33
aayemm98 <begaitj8@gmail.com>
2023-07-01 16:48:07 +0600

    pr

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import Home from "./companents/Home";
 import Login from "./companents/Login/login";
 import Agents from "./companents/Agents/agents";
 import Reviews from "./companents/Reviews/reviews"
+import Profile from "./companents/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route path={'/'} element={<Home/>}/>
-        <Route path={'/login'} element={<Login/>}/>
-        <Route path={'/agents'} element={<Agents/>}/>
-        <Route path={'/reviews'} element={<Reviews/>}/>
-      </Routes>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Profile/>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/agents'} element={<Agents/>}/>
+                <Route path={'/reviews'} element={<Reviews/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
